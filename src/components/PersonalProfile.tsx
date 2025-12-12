c54741fd6d9518673223c4f375a51541c93a15a3
alex luo <5525899@qq.com>
2025-12-12 09:50:07 +0800

    refactor(PersonalProfile): remove unused react-icons imports
    Remove FaReact and FaServer icons that were not being used in the component to clean up dependencies

--- src/components/PersonalProfile.tsx
+++ src/components/PersonalProfile.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FaReact, FaServer, FaRobot, FaCode, FaGraduationCap, FaPalette } from 'react-icons/fa';
+import { FaRobot, FaCode, FaGraduationCap, FaPalette } from 'react-icons/fa';
 
 interface PersonalProfileProps {
   onClose: () => void;
